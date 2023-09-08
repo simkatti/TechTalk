5f82c07eea0406e770b62d9ebabb784c916a4267
Callegari Simona <csimona@cubbli22-gold.local>
2023-09-08 19:28:23 +0300

    Database and tables created. Sign up page done succesfully. Fixed some error in html files.

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
-from flask import render_template, request
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = getenv("SECRET_KEY")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -11,13 +19,32 @@ def index():
 def page1():
     return render_template("login.html")
 
-@app.route("/create_account")
+@app.route("/create_account", methods=["GET", "POST"])
 def page2():
-    return render_template("createaccount.html")
+    passwords_match = True
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        password1 = request.form["password1"]
 
-@app.route("/home_page", methods=["POST"])
+        if not password or not password1:
+            passwords_match = False
+        elif password == password1:
+            session["username"] = username
+            hash_value = generate_password_hash(password)
+            sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
+            db.session.execute(sql, {"username":username, "password":hash_value})
+            db.session.commit()
+            return redirect("/home_page")
+        else:
+            passwords_match = False
+        
+
+    return render_template("createaccount.html", passwords_match=passwords_match)
+
+@app.route("/home_page", methods=["GET", "POST"])
 def page3():
-    return render_template("homepage.html", name=request.form["username"])
+    return render_template("homepage.html", name = session.get("username"))
 
 @app.route("/hub_pages")
 def page4():
