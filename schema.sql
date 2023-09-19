d95f8984778a1d2aa0ff520740b72b634818e195
Callegari Simona <csimona@cubbli22-gold.local>
2023-09-19 20:16:16 +0300

    added database structure schema.sql

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    password TEXT,
+);
+
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE rooms (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    category_id INTEGER REFERENCES categories(id)
+);
+
+CREATE TABLE chats (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    room_id INTEGER REFERENCES rooms(id)
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    message TEXT,
+    time TIMESTAMP,
+    chat_id INTEGER REFERENCES chats(id)
+    user_id INTEGER REFERENCES users(id)
+)
