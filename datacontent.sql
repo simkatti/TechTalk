6d21a3bc7d9fbb5c38285642e30cf647a1cebcff
Callegari Simona <csimona@cubbli22-gold.local>
2023-09-27 14:25:36 +0300

    Added datacontent.sql for better testing

---
+++ datacontent.sql
@@ -0,0 +1,32 @@
+INSERT INTO categories (name)
+VALUES
+    ('Bachelor'),
+    ('Master'),
+    ('Doctor'),
+    ('Courses'),
+    ('Games'),
+    ('Random'),
+    ('Party'),
+    ('Sports');
+
+INSERT INTO rooms (name, category_id)
+VALUES
+    ('lukujärjestys', 1),
+    ('sivuaine', 1),
+    ('helpot nopat', 1),
+    ('graduttaa', 2),
+    ('maisterilinja', 2),
+    ('väikkäri', 3),
+    ('JYM', 4),
+    ('tsoha', 4),
+    ('tikape', 4),
+    ('ohpe & ohja', 4),
+    ('tito', 4),
+    ('sims', 5),
+    ('gta', 5),
+    ('testihuone', 6),
+    ('sitsit', 7),
+    ('fuksileiri', 7),
+    ('unisport', 8);
+
+
